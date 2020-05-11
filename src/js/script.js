da8997421b9f5b4b01b583ba664385adcb2d174e
Yuri <nemirichyuri@gmail.com>
2020-05-11 22:15:49 +0300

    step 1 js

---
+++ src/js/script.js
@@ -0,0 +1,63 @@
+var name = 'Ivan';
+let number = 7;
+const pi = 3.14;
+number = 4;
+let leftBorderWidth = 200;
+
+// number - числа
+// string - "", '', ``
+// true / false
+// null
+// undefined
+// let obj = {
+//   name: 'apple',
+//   color: 'green',
+//   weight: 200,
+// };
+// Symbol
+
+// alert(1234);
+// console.log(number);
+// let answer = confirm('Вам есть 18?');
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?', '');
+// console.log(answer);
+
+// console.log(4 + 4);
+// let isChecked = true,
+//   isClose = false;
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+// if (2 * 4 == 6 * 1) {
+//   console.log('верно');
+// } else {
+//   console.log('ошибка');
+// }
+// let answer = confirm('Вам есть 18?');
+// if (answer) {
+//   console.log('проходите');
+// } else {
+//   console.log('уходи');
+// }
+
+// const num = 50;
+// if (num < 49) {
+//   console.log('неправильно');
+// } else if (num > 100) {
+//   console.log('много');
+// } else {
+//   console.log('верно');
+// }
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(i);
+// }
+
+function logging(a, b) {
+  console.log(a + b);
+}
+logging(3, 5);
+
+logging(4, 10);
