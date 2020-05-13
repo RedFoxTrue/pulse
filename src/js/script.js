facec0e0387d32cb30e63b153f9826fb2513e27d
Yuri <nemirichyuri@gmail.com>
2020-05-13 19:47:44 +0300

    step 3 without homework

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,130 @@
-var name = 'Ivan';
-let number = 7;
-const pi = 3.14;
-number = 4;
-let leftBorderWidth = 200;
+$(document).ready(function () {
+  $('ul.catalog__tabs').on(
+    'click',
+    'li:not(.catalog__tab_active)',
+    function () {
+      $(this)
+        .addClass('catalog__tab_active')
+        .siblings()
+        .removeClass('catalog__tab_active')
+        .closest('div.container')
+        .find('div.catalog__content')
+        .removeClass('catalog__content_active')
+        .eq($(this).index())
+        .addClass('catalog__content_active');
+    },
+  );
+  var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    // slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+
+  // $('.catalog-item__link').each(function (i) {
+  //   $(this).on('click', function (e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   })
+  // });
+
+  // $('.catalog-item__back').each(function (i) {
+  //   $(this).on('click', function (e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   })
+  // });
+
+//   function toggleSlide(item) {
+//     $(item).each(function(i) {
+//         $(this).on('click', function(e) {
+//             e.preventDefault();
+//             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+//             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+//         })
+//     });
+// };
+
+// toggleSlide('.catalog-item__link');
+// toggleSlide('.catalog-item__back');
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//   slider.goTo('prev');
+// });
+// document.querySelector('.next').addEventListener('click', function () {
+//   slider.goTo('next');
+// });
+// // (function ($) {
+// //   $(function () {
+// //     $('ul.catalog__tabs').on(
+// //       'click',
+// //       'li:not(.catalog__tab_active)',
+// //       function () {
+// //         $(this)
+// //           .addClass('catalog__tab_active')
+// //           .siblings()
+// //           .removeClass('catalog__tab_active')
+// //           .closest('div.container')
+// //           .find('div.catalog__content')
+// //           .removeClass('catalog_content_active')
+// //           .eq($(this).index())
+// //           .addClass('catalog_content_active');
+// //       },
+// //     );
+// //   });
+// // })(jQuery);
+// var name = 'Ivan';
+// let number = 7;
+// const pi = 3.14;
+// number = 4;
+// let leftBorderWidth = 200;
+
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     speed: 1200,
+//     // adaptiveHeight: true,
+//     prevArrow:
+//       '<button type="button" class="slick-prev"><img src="../img/slider_clock/chevron-left-solid.png"></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"><img src="../img/slider_clock/chevron-right-solid.png"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           dots: true,
+//         },
+//       },
+//     ],
+//   });
+// });
+
+// tiny-slider:
 
 // number - числа
 // string - "", '', ``
@@ -55,9 +177,9 @@ let leftBorderWidth = 200;
 //   console.log(i);
 // }
 
-function logging(a, b) {
-  console.log(a + b);
-}
-logging(3, 5);
+// function logging(a, b) {
+//   console.log(a + b);
+// }
+// logging(3, 5);
 
-logging(4, 10);
+// logging(4, 10);
